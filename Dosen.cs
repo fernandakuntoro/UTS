b36697284d51bfe6c19709676f51f3fd3c253e35
fernandakuntoro <fernandahizkia11@students.amikom.ac.id>
2022-05-24 08:44:28 +0700

    commit

---
+++ Dosen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_PEMROGRAMAN
+{
+    class Dosen
+    {
+        public int id { get; set; }
+        public string nama { get; set; }
+        public int Nik { get; set; }
+        public string gender { get; set; }
+        public string course { get; set; }
+
+        public void dosen()
+        {
+            Console.WriteLine("Tahun Ajaran \t: {0}", id);
+            Console.WriteLine("Nama Dosen \t: {0}", nama);
+            Console.WriteLine("NIK Kuliah \t: {0}", Nik);
+            Console.WriteLine("Jenis Kelamin \t: {0}", gender);
+            Console.WriteLine("Mata Kuliah \t: {0}", course);
+        }
+        public static void attdosen(string name, int nik)
+        {
+            Console.WriteLine("Nama Dosen \t: {0}", name);
+            Console.WriteLine("NIK Dosen \t: {0}", nik);
+        }
+        public static void addcourse(string course)
+        {
+            Console.WriteLine("Mata Kuliah \t: {0}", course);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_PEMROGRAMAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama, course;
+            int nik;
+
+            Dosen tetap = new Dosen();
+            tetap.id = 21;
+            tetap.nama = "Kamarudin, M.Kom";
+            tetap.Nik = 192021;
+            tetap.gender = "Laki-laki";
+            tetap.course = "Pemrograman";
+
+            Console.WriteLine("===== DATA DOSEN DEFAULT =====");
+            Console.WriteLine();
+            tetap.dosen();
+            Console.WriteLine();
+
+            Console.WriteLine("===== INPUT DATA DOSEN BARU =====");
+            Console.WriteLine();
+            Console.Write("Masukkan Nama Dosen \t\t: ");
+            nama = Console.ReadLine();
+
+            Console.Write("Masukkan NIK Dosen \t\t: ");
+            nik = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan Mata Kuliah Dosen \t: ");
+            course = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("===== DATA DOSEN YANG SUDAH DIINPUTKAN =====");
+            Console.WriteLine();
+            Dosen.attdosen(nama, nik);
+            Dosen.addcourse(course);
+
+            Console.ReadKey();
+        }
+    }
+}
